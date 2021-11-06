c062e5d381cd65b9aeb147746179d37cf6a9b67e
slawektoja <kobylko996@o2.pl>
2021-11-06 13:23:01 +0100

    06.11 13:22

--- src/com/devices/Device.java
+++ src/com/devices/Device.java
@@ -1,5 +1,14 @@
 package com.devices;
 
 public class Device {
+    final public String producer;
+    final public String model;
+    final public Integer yearOfProduction;
+    public Double value;
 
+    public Device(String producer, String model, Integer yearOfProduction) {
+        this.producer = producer;
+        this.model = model;
+        this.yearOfProduction = yearOfProduction;
+    }
 }
